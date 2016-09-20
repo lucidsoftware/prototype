1e3cb49395fed3831d9f9c5bbc25e7b3c3a845cf
corysmc <cory@lucidchart.com>
2016-09-20 14:15:44 -0600

    initial commit of android

---
+++ mobile/android/1.0/js/controllers.js
@@ -0,0 +1,111 @@
+angular.module('lucidMobile.controllers', [])
+    .controller('AppCtrl', ['$scope', '$ionicModal', '$timeout', function($scope, $ionicModal, $timeout) {
+
+        // With the new view caching in Ionic, Controllers are only called
+        // when they are recreated or on app start, instead of every page change.
+        // To listen for when this page is active (for example, to refresh data),
+        // listen for the $ionicView.enter event:
+        //$scope.$on('$ionicView.enter', function(e) {
+        //});
+
+        // Form data for the login modal
+        $scope.loginData = {};
+
+        // Create the login modal that we will use later
+        $ionicModal.fromTemplateUrl('templates/login.html', {
+            scope: $scope
+        }).then(function(modal) {
+            $scope.modal = modal;
+        });
+
+        // Triggered in the login modal to close it
+        $scope.closeLogin = function() {
+            $scope.modal.hide();
+        };
+
+        // Open the login modal
+        $scope.login = function() {
+            $scope.modal.show();
+        };
+
+        // Perform the login action when the user submits the login form
+        $scope.doLogin = function() {
+            console.log('Doing login', $scope.loginData);
+            // Simulate a login delay. Remove this and replace with your login
+            // code if using a login system
+            $timeout(function() {
+                $scope.closeLogin();
+            }, 1000);
+        };
+    }])
+    .controller('documentsCtrl', ['$scope', 'documents', 'folders', '$stateParams', '$ionicActionSheet',
+        function($scope, documents, folders, $stateParams, $ionicActionSheet) {
+            if ($stateParams.folderID) {
+                //console.log(folders.getByID($stateParams.folderID));
+                $scope.folderName = folders.getByID($stateParams.folderID).name;
+                $scope.folders = folders.getFromFolder($stateParams.folderID);
+                $scope.documents = documents.getFromFolder($stateParams.folderID);
+            } else {
+                $scope.folderName = 'My Documents';
+                $scope.documents = documents.getFromFolder(0);
+                $scope.folders = folders.getFromFolder(0);
+            }
+            $scope.showDrawer = function() {
+
+                // Show the action sheet
+                $ionicActionSheet.show({
+                    buttons: [
+                        { text: '<i class="icon material-icons">text_format</i>Rename' },
+                        { text: '<i class="icon material-icons">content_copy</i>Copy' },
+                        { text: '<i class="icon material-icons">folder</i>Move' },
+                        { text: '<i class="icon material-icons">people</i>Share' }
+                    ],
+                    destructiveText: '<i class="icon material-icons">delete</i>Delete',
+                    cancel: function() {
+                        // add cancel code..
+                    },
+                    buttonClicked: function(index) {
+                        console.log(index);
+                        return true;
+                    },
+                    destructiveButtonClicked: function() {
+                        console.log('delete');
+                        return true;
+                    }
+                });
+
+                // For example's sake, hide the sheet after two seconds
+
+            };
+
+        }
+    ])
+    .controller('documentCtrl', ['$scope', '$stateParams', 'documents', '$rootScope', function($scope, $stateParams, documents, $rootScope) {
+        $scope.currentDocument = documents.getByID($stateParams.documentID);
+        $rootScope.currentPage = documents.getByID($stateParams.documentID).pages[0];
+    }])
+    .controller('shapeManagerCtrl', ['$scope', 'lucidShapesData', '$rootScope', '$ionicModal', function($scope, lucidShapesData, $rootScope, $ionicModal) {
+        $scope.pinnedShapeGroups = lucidShapesData.lucidShapeGroups();
+        $scope.search = {};
+        $scope.showShapeManager = function() {
+            //console.log('manage shapes');
+            var $scope = $rootScope.$new();
+            $ionicModal.fromTemplateUrl('templates/shape-manager-modal.html', {
+                scope: $scope,
+                animation: 'slide-in-up',
+                focusFirstInput: true
+            }).then(function(modal) {
+                $scope.modal = modal;
+                $scope.modal.show();
+
+
+
+
+                $scope.cancel = function() {
+                    $scope.modal.hide().then(function() {
+                        $scope.modal.remove();
+                    });
+                };
+            });
+        };
+    }]);
---
+++ mobile/android/1.0/js/services.js
@@ -0,0 +1,129 @@
+angular.module('lucidMobile.services', [])
+    .factory('documents', ['$filter', function($filter) {
+        var documents = [{
+            id: 0,
+            name: 'Doc Title',
+            insideFolder: 0,
+            pages: [{
+                'name': 'Page 1',
+                'thumb': 'assets/thumb-placeholder-flowchart.png',
+                'id': 14543,
+                'canvasBG': '#FFF',
+                'blocks': [{
+                    "customcolor": false,
+                    "swatch": {
+                        "fill": "#ffffff",
+                        "text": "#AEAEAE",
+                        "border": "#666666"
+                    },
+                    "swatchnumber": 1,
+                    "borderwidth": 3,
+                    "borderstyle": "solid",
+                    "comment": {
+                        "text": "yep this is a comment"
+                    },
+                    "table": true,
+                    "text": {
+                        "verticalalignment": "middle",
+                        "horizontalalignment": "center",
+                        "text": "Process",
+                        "size": 16,
+                    },
+
+                    "padding": 7,
+                    "metrics": {
+                        "z": 2,
+                        "x": 75,
+                        "y": 75,
+                        "width": 120,
+                        "height": 100
+                    },
+                    "svg": '<svg width="30px" height="30px" viewBox="0 0 30 30" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g class="lucid-shapes-fill-stroke" sketch:type="MSArtboardGroup" stroke="#979797" stroke-width="2" fill="#FFFFFF"><rect id="Rectangle-32-Copy-3" sketch:type="MSShapeGroup" x="1" y="2" width="28" height="26" rx="2"></rect></g>'
+
+                }, {
+                    "customcolor": false,
+                    "swatch": {
+                        "fill": "#ffffff",
+                        "text": "#AEAEAE",
+                        "border": "#666666"
+                    },
+                    "swatchnumber": 1,
+                    "borderwidth": 3,
+                    "borderstyle": "solid",
+                    "text": {
+                        "verticalalignment": "middle",
+                        "horizontalalignment": "center",
+                        "text": "Process",
+                        "size": 16,
+                    },
+
+                    "padding": 7,
+                    "metrics": {
+                        "z": 2,
+                        "x": 75,
+                        "y": 243,
+                        "width": 120,
+                        "height": 80
+                    },
+
+
+                }],
+                'lines': [{
+                    "points": "450.5 184 450.5 240",
+                    "endarrow": true,
+                    "width": 3,
+                }],
+                'linecolor': '#333'
+            }]
+        }, {
+            id: 1,
+            name: 'Doc 2 title',
+            insideFolder: 1
+        }];
+        var documentFunctions = {
+            all: function() {
+                return documents;
+            },
+            getByID: function(documentID) {
+                var filterDocs = $filter('filter')(documents, { id: documentID });
+                return filterDocs[0];
+            },
+            getFromFolder: function(folderID) {
+                return $filter('filter')(documents, { insideFolder: folderID });
+            }
+        };
+        return documentFunctions;
+    }])
+    .factory('folders', ['$filter', function($filter) {
+        var folders = [{
+            id: 1,
+            name: 'UX Group',
+            insideFolder: 0,
+            shared: true
+        }, {
+            id: 2,
+            name: 'My Folder',
+            insideFolder: 0
+        }, {
+            id: 3,
+            name: 'Android Concepts',
+            insideFolder: 1
+        }, {
+            id: 4,
+            name: '1.0',
+            insideFolder: 3
+        }];
+        var folderFunctions = {
+            all: function() {
+                return folders;
+            },
+            getByID: function(folderID) {
+                var filterFolders = $filter('filter')(folders, { id: folderID });
+                return filterFolders[0];
+            },
+            getFromFolder: function(folderID) {
+                return $filter('filter')(folders, { insideFolder: folderID });
+            }
+        };
+        return folderFunctions;
+    }]);
